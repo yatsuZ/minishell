83a2c271af29814568a8c1c173da52dd412ddd48
yatsuZ <y.zaoui.pro@gmail.com>
2023-11-14 15:57:11 +0100

    Avancement

---
+++ code/1/Source/main.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yzaoui <yzaoui@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 03:10:01 by yzaoui            #+#    #+#             */
+/*   Updated: 2023/11/11 03:22:53 by yzaoui           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./../Header/Minishell.h"
+
+int	main(void)
+{
+	if (TRUE)
+		printf("Anonce importante : \n");
+	printf("Hello World !!!\nNew project coming with\nYassine and Ilham!\n");
+	return (0);
+}
