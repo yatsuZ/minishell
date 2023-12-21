a35ec07747de8715d64dffc901fa52cc27a89bec
yatsuZ <yassine.zaoui28062003@gmail.com>
2023-12-21 11:53:23 +0100

    Instruction

--- code/Ebauche/Source/parsing/parsing.c
+++ code/Ebauche/Source/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: yzaoui <yzaoui@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 00:52:43 by yzaoui            #+#    #+#             */
-/*   Updated: 2023/12/21 11:40:26 by yzaoui           ###   ########.fr       */
+/*   Updated: 2023/12/21 11:50:33 by yzaoui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 
 // Fonction qui va permertre de defnir quelle sont les type d'inpute
+// Verifier les cote puis double cote entre les deux c'est des string
+// Verifier les variable global
+// Verifier les redirection 
+// Verifier les pip
+// Modifier les variable global et modifier les str (refaire un decoupage en gros)
+// Verifier les cmd
+// Verfier les syntax
 void	define_type_input(t_all_struct *all)
 {
 	return ;
