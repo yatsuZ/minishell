05993bf9cac788e6277c72d298ab8a1cb2ce5a6a
yatsuZ <y.zaoui.pro@gmail.com>
2024-01-09 18:50:24 +0100

    Correction pour le R_IN_LIMIT

---
+++ code/EXECUTION_ILHAM/EBAUCHE_EXECUTION_YASSINE/Source/inutile/color.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yzaoui <yzaoui@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/12 22:07:18 by yzaoui            #+#    #+#             */
+/*   Updated: 2023/12/26 19:51:30 by yzaoui           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../Header/Minishell.h"
+
+void	show_color(void)
+{
+	printf("\033[31mRouge\033[0m\n");
+	printf("\033[32mVert\033[0m\n");
+	printf("\033[33mJaune\033[0m\n");
+	printf("\033[34mBleu\033[0m\n");
+	printf("\033[35mMagenta\033[0m\n");
+	printf("\033[36mCyan\033[0m\n");
+	printf("\033[37mBlanc\033[0m\n");
+}
+
+void	put_color_txt(int color)
+{
+	if (color == ROUGE)
+		printf("\033[31m");
+	else if (color == VERT)
+		printf("\033[32m");
+	else if (color == JAUNE)
+		printf("\033[33m");
+	else if (color == BLEU)
+		printf("\033[34m");
+	else if (color == MAGENTA)
+		printf("\033[35m");
+	else if (color == CYAN)
+		printf("\033[36m");
+	else if (color == BLANC)
+		printf("\033[37m");
+	else
+		printf("\033[0m");
+}
