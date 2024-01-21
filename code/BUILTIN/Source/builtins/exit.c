42c6f1107b2cbf99554b033fa9dffae65d0a4454
YatsuZ <yassine.zaoui28062003@gmail.com>
2024-01-21 01:53:33 +0100

    ft exit fais

--- code/BUILTIN/Source/builtins/exit.c
+++ code/BUILTIN/Source/builtins/exit.c
@@ -6,38 +6,56 @@
 /*   By: yzaoui <yzaoui@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 17:59:44 by yzaoui            #+#    #+#             */
-/*   Updated: 2024/01/20 18:17:12 by yzaoui           ###   ########.fr       */
+/*   Updated: 2024/01/21 01:50:26 by yzaoui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../../Header/Minishell.h"
-// LLONG_MIN
-// LLONG_MAX
-// long long
-int	is_numeric(char *str)
-{
-	unsigned long long	nbr;
-	size_t				i;
 
-	i = 0;
+static long long	is_numeric2(char *str, size_t i, int *err)
+{
+	long long	nbr;
+
 	nbr = 0;
+	if (str[i] == '\0')
+		return (*err = -2, 0);
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		if (str[0] == '-')
+		{
+			nbr = nbr * 10 - (str[i] - '0');
+			if (nbr > 0)
+				return (*err = -2, 0);
+		}
+		else
+		{
+			nbr = nbr * 10 + (str[i] - '0');
+			if (nbr < 0)
+				return (*err = -2, 0);
+		}
+		i++;
+	}
+	if (str[i] != '\0')
+		return (*err = -2, 0);
+	return (nbr);
+}
+
+int	str_to_modul255(char *str, size_t i)
+{
+	long long	nbr;
+	int			err;
+
+	err = 0;
 	if (!str)
 		return (-1);
 	if (str[i] == '-' || str[i] == '+')
 		i++;
-	if (str[i] == '\0')
+	nbr = is_numeric2(str, i, &err);
+	if (err == -2)
 		return (-2);
-	while (str[i] && (str[i] >= '0' && str[i] <= '9') && nbr <= LONG_MAX)
-	{
-		nbr = nbr * 10 + (str[i] - '0');
-		i++;
-	}
-	if (str[i] != '\0' || nbr > LONG_MAX)
-		return (-2);
-	if (str[0] == '-')
-		nbr = nbr * -1;
-	return ((int) (nbr % 255));
-	
+	if (str[0] == '-' && nbr != 0)
+		return (256 - (nbr % 256));
+	return (nbr % 256);
 }
 
 int	exec_exit(t_execute *exe, t_all_struct **all)
@@ -51,7 +69,7 @@ int	exec_exit(t_execute *exe, t_all_struct **all)
 		print_fd("bash: exit: too many arguments\n", 2);
 		return (1);
 	}
-	res = is_numeric(exe->arg[0]);
+	res = str_to_modul255(exe->arg[0], 0);
 	if (res == -2)
 	{
 		msg = ft_strjoin("bash: exit: ", exe->arg[0]);
