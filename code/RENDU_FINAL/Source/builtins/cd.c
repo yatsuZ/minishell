c3da9d304674300d5490f13a9a51f72496586e10
yatsuZ <y.zaoui.pro@gmail.com>
2024-02-09 21:11:16 +0100

    pas derreur

---
+++ code/RENDU_FINAL/Source/builtins/cd.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yzaoui <yzaoui@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 17:56:19 by yzaoui            #+#    #+#             */
+/*   Updated: 2024/02/09 21:09:06 by yzaoui           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./../../Header/Minishell.h"
+
+t_boolean	i_can_show_va(t_env *env, char *str)
+{
+	while (env && str)
+	{
+		if (ft_strcpm(env->key, str))
+			return (env->show);
+		env = env->next_va;
+	}
+	return (FALSE);
+}
+
+t_boolean	key_exist(t_env *env, char *str)
+{
+	while (env)
+	{
+		if (ft_strcpm(env->key, str))
+			return (TRUE);
+		env = env->next_va;
+	}
+	return (FALSE);
+}
+
+char	*get_first_arg_no_null(char **arv, size_t arc, size_t i)
+{
+	while (i < arc)
+	{
+		if (arv[i] != NULL)
+			return (ft_strdup(arv[i]));
+		i++;
+	}
+	return (NULL);
+}
+
+static int	exec_cd2(char *chemin, t_all_struct **all)
+{
+	char		*tmp;
+	char		*tmp2;
+	t_boolean	show;
+
+	tmp2 = get_value((*all)->all_va, "PWD", (*all)->status);
+	show = i_can_show_va((*all)->all_va, "OLDPWD");
+	if (key_exist((*all)->all_va, "PWD"))
+		change_or_add_va(&((*all)->all_va), "OLDPWD", tmp2, show);
+	tmp = getcwd(NULL, 0);
+	show = i_can_show_va((*all)->all_va, "PWD");
+	change_or_add_va(&((*all)->all_va), "PWD", tmp, show);
+	return (free_2str(&tmp, &tmp2), free(chemin), chemin = NULL, 0);
+}
+
+int	exec_cd(t_execute *exe, t_all_struct **all)
+{
+	char		*chemin;
+
+	chemin = get_first_arg_no_null(exe->arg, exe->argc, 1);
+	if (!chemin)
+		chemin = get_value((*all)->all_va, "HOME", (*all)->status);
+	if (!chemin)
+		return (print_fd("Minishell error: cd: HOME not set\n", 2), 1);
+	if (chemin[0] != '\0' && chdir(chemin))
+		return (print_fd("Minishell error: cd: \
+chemin non existant OU non les droits\n", 2), free(chemin), chemin = NULL, 1);
+	return (exec_cd2(chemin, all));
+}
---
+++ code/RENDU_FINAL/Source/builtins/export.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yzaoui <yzaoui@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 17:57:47 by yzaoui            #+#    #+#             */
+/*   Updated: 2024/02/09 21:06:43 by yzaoui           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./../../Header/Minishell.h"
+
+static void	print_env_export(t_env *all_va)
+{
+	if (!all_va)
+		return ;
+	if (all_va->show)
+	{
+		if (all_va->value)
+			printf("export %s=\"%s\"\n", all_va->key, all_va->value);
+		else
+			printf("export %s\n", all_va->key);
+	}
+	print_env_export(all_va->next_va);
+}
+
+static char	*get_value_from_str(char *str, size_t stop, int *err)
+{
+	char	*res;
+	size_t	start;
+
+	if (*err)
+		return (NULL);
+	start = stop + 1;
+	while (str[stop])
+		stop++;
+	res = ft_strcut(str, start, stop);
+	return (res);
+}
+
+int	ft_export(char *str, t_env **all_env, size_t stop, int *err)
+{
+	char	*key;
+	char	*value;
+
+	while (str && str[stop] && str[stop] != '=')
+		stop++;
+	key = ft_strcut(str, 0, stop);
+	if (!*err && (is_a_legit_va_env(key) == FALSE || \
+	ft_strcpm("?", key) == TRUE))
+	{
+		print_fd("Minishelle : export: `", 2);
+		if (key)
+			print_fd(key, 2);
+		print_fd("\': key non valide\n", 2);
+		return (free_2str(&key, NULL), value = NULL, 1);
+	}
+	if (str[stop] == '\0')
+		value = NULL;
+	else if (str[stop + 1] == '\0')
+		value = ft_strdup("");
+	else
+		value = get_value_from_str(str, stop, err);
+	return (change_or_add_va(all_env, key, value, 1), \
+	free_2str(&key, &value), 0);
+}
+
+int	exec_export(t_execute *exe, t_all_struct **all)
+{
+	int		status;
+	size_t	i;
+
+	i = 1;
+	status = 0;
+	if (exe->argc == 1)
+		return (print_env_export((*all)->all_va), status);
+	while (i < exe->argc)
+	{
+		if (ft_export(exe->arg[i], &(*all)->all_va, 0, &((*all)->err)))
+			status = 1;
+		if ((*all)->err)
+			end(*all);
+		i++;
+	}
+	update_env(all);
+	return (status);
+}
